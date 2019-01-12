19f4d7537cbb86d96c55cfce92a6ad4a73c7d1e2
bpelikan <bartoszpelikan@gmail.com>
2019-01-13 00:32:09 +0100

    .

--- NotificationFunctions/NotificationFunctions/Shared/AppointmentReminderMessage.cs
+++ NotificationFunctions/NotificationFunctions/Shared/AppointmentReminderMessage.cs
@@ -7,11 +7,13 @@ namespace Shared
     public class AppointmentReminderMessage
     {
         public string Email { get; set; }
-        public string InterviewAppointmentId { get; set; }
-        public string JobPositionName { get; set; }
         public DateTime NotificationTime { get; set; }
         public DateTime StartTime { get; set; }
         public int Duration { get; set; }
         public DateTime EndTime { get; set; }
+        public string InterviewAppointmentId { get; set; }
+        public string JobPositionName { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
     }
 }
