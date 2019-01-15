89e25a1e59f2509f1dcac5355828ec6ce8b06bc0
bpelikan <bartoszpelikan@gmail.com>
2019-01-15 23:12:01 +0100

    Update GenerateAppointmentReminder function

--- NotificationFunctions/NotificationFunctions/NotificationFunctions/GenerateAppointmentReminder.cs
+++ NotificationFunctions/NotificationFunctions/NotificationFunctions/GenerateAppointmentReminder.cs
@@ -11,55 +11,71 @@ namespace NotificationFunctions
     public static class GenerateAppointmentReminder
     {
         [FunctionName("GenerateAppointmentReminder")]
-        public async static Task Run(
-            [QueueTrigger("generateappointmentreminderqueue", Connection = "GenerateAppointmentReminderQueueConnectionString")]string myQueueItem,
-            [Queue("processappointmentreminderqueue", Connection = "ProcessAppointmentReminderQueuequeueConnectionString")]CloudQueue outputQueue,
-            ILogger log)
+        public async static Task Run([QueueTrigger("generateappointmentreminderqueue", Connection = "GenerateAppointmentReminderQueueConnectionString")]string generateAppointmentReminderItem, [Queue("processappointmentreminderqueue", Connection = "ProcessAppointmentReminderQueuequeueConnectionString")]CloudQueue processAppointmentReminderQueue)
         {
-            log.LogInformation($"C# Queue trigger function GenerateAppointmentReminder processed:\n{myQueueItem}");
-
-            var data = JsonConvert.DeserializeObject<AppointmentReminderMessage>(myQueueItem);
-            log.LogInformation( $"\nEmail:              {data.Email}" +
-                                $"\nJobPositionName:    {data.JobPositionName}" +
-                                $"\nNowTime:            {DateTime.UtcNow.ToLocalTime()}" +
-                                $"\nNotificationTime:   {data.NotificationTime.ToLocalTime()}" +
-                                $"\nStartTime:          {data.StartTime.ToLocalTime()}");
-
+            var notificationData = JsonConvert.DeserializeObject<AppointmentReminderMessage>(generateAppointmentReminderItem);
             TimeSpan invisibleTime = TimeSpan.FromMinutes(0);
-            if (DateTime.UtcNow <= data.NotificationTime)
-            {
-                if ((data.NotificationTime - DateTime.UtcNow) <= TimeSpan.FromDays(StaticValue.maxInvisibleTime))
-                {
-                    invisibleTime = data.NotificationTime - DateTime.UtcNow;
-                    log.LogWarning(     $"\n1:------------------1------------------" +
-                                        $"\n1:------------------invisibleTime: {invisibleTime}------------------");
-                }
+            if (DateTime.UtcNow <= notificationData.NotificationTime)
+                if ((notificationData.NotificationTime - DateTime.UtcNow) <= TimeSpan.FromDays(StaticValue.maxInvisibleTime))
+                    invisibleTime = notificationData.NotificationTime - DateTime.UtcNow;
                 else
-                {
                     invisibleTime = TimeSpan.FromDays(StaticValue.maxInvisibleTime);
-                    log.LogWarning(     $"\n1:------------------2------------------" +
-                                        $"\n1:------------------invisibleTime: {invisibleTime}------------------");
-                }
-            }   
-            else
-            {
-                log.LogWarning( $"\n1:------------------3-------------------" +
-                                $"\n1:------------------invisibleTime: {invisibleTime}------------------");
-            }
 
-            await outputQueue.CreateIfNotExistsAsync();
-            var queueMessage = new CloudQueueMessage(myQueueItem);
-            await outputQueue.AddMessageAsync(
-                queueMessage,
-                timeToLive: null,
-                initialVisibilityDelay: invisibleTime,
-                options: null,
-                operationContext: null);
-
-            //dynamic data = JsonConvert.DeserializeObject(myQueueItem);
-            //var data = JsonConvert.DeserializeObject<AppointmentReminderMessage>(myQueueItem);
-            //log.LogInformation($@"NotificationTime: {data.NotificationTime}/n
-            //                      StartTime: {data.StartTime}");
+            await processAppointmentReminderQueue.CreateIfNotExistsAsync();
+            var notificationMessage = new CloudQueueMessage(generateAppointmentReminderItem);
+            await processAppointmentReminderQueue.AddMessageAsync(notificationMessage, null, invisibleTime, null, null);
         }
+
+        //[FunctionName("GenerateAppointmentReminder")]
+        //public async static Task Run(
+        //    [QueueTrigger("generateappointmentreminderqueue", Connection = "GenerateAppointmentReminderQueueConnectionString")]string myQueueItem,
+        //    [Queue("processappointmentreminderqueue", Connection = "ProcessAppointmentReminderQueuequeueConnectionString")]CloudQueue outputQueue,
+        //    ILogger log)
+        //{
+        //    log.LogInformation($"C# Queue trigger function GenerateAppointmentReminder processed:\n{myQueueItem}");
+
+        //    var data = JsonConvert.DeserializeObject<AppointmentReminderMessage>(myQueueItem);
+        //    log.LogInformation( $"\nEmail:              {data.Email}" +
+        //                        $"\nJobPositionName:    {data.JobPositionName}" +
+        //                        $"\nNowTime:            {DateTime.UtcNow.ToLocalTime()}" +
+        //                        $"\nNotificationTime:   {data.NotificationTime.ToLocalTime()}" +
+        //                        $"\nStartTime:          {data.StartTime.ToLocalTime()}");
+
+        //    TimeSpan invisibleTime = TimeSpan.FromMinutes(0);
+        //    if (DateTime.UtcNow <= data.NotificationTime)
+        //    {
+        //        if ((data.NotificationTime - DateTime.UtcNow) <= TimeSpan.FromDays(StaticValue.maxInvisibleTime))
+        //        {
+        //            invisibleTime = data.NotificationTime - DateTime.UtcNow;
+        //            log.LogWarning(     $"\n1:------------------1------------------" +
+        //                                $"\n1:------------------invisibleTime: {invisibleTime}------------------");
+        //        }
+        //        else
+        //        {
+        //            invisibleTime = TimeSpan.FromDays(StaticValue.maxInvisibleTime);
+        //            log.LogWarning(     $"\n1:------------------2------------------" +
+        //                                $"\n1:------------------invisibleTime: {invisibleTime}------------------");
+        //        }
+        //    }   
+        //    else
+        //    {
+        //        log.LogWarning( $"\n1:------------------3-------------------" +
+        //                        $"\n1:------------------invisibleTime: {invisibleTime}------------------");
+        //    }
+
+        //    await outputQueue.CreateIfNotExistsAsync();
+        //    var queueMessage = new CloudQueueMessage(myQueueItem);
+        //    await outputQueue.AddMessageAsync(
+        //        queueMessage,
+        //        timeToLive: null,
+        //        initialVisibilityDelay: invisibleTime,
+        //        options: null,
+        //        operationContext: null);
+
+        //    //dynamic data = JsonConvert.DeserializeObject(myQueueItem);
+        //    //var data = JsonConvert.DeserializeObject<AppointmentReminderMessage>(myQueueItem);
+        //    //log.LogInformation($@"NotificationTime: {data.NotificationTime}/n
+        //    //                      StartTime: {data.StartTime}");
+        //}
     }
 }
