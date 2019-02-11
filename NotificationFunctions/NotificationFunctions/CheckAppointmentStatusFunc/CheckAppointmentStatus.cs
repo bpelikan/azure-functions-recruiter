a2bd30f7406cbf24e18394a9a1cdd564e1966c65
bpelikan <bartoszpelikan@gmail.com>
2019-02-11 19:26:18 +0100

    Update CheckAppointmentStatus function

--- NotificationFunctions/NotificationFunctions/CheckAppointmentStatusFunc/CheckAppointmentStatus.cs
+++ NotificationFunctions/NotificationFunctions/CheckAppointmentStatusFunc/CheckAppointmentStatus.cs
@@ -15,34 +15,24 @@ namespace CheckAppointmentStatusFunc
     {
         [FunctionName("CheckAppointmentStatus")]
         public async static Task Run(
-            [QueueTrigger("checkappointmentstatusqueue", Connection = "CheckAppointmentStatusQueueConnectionString")]string myQueueItem,
-            [Queue("sendappointmentreminderqueue", Connection = "SendAppointmentReminderQueueConnectionString")]CloudQueue outputQueue,
+            [QueueTrigger("checkappointmentstatusqueue", Connection = "CheckAppointmentStatusQueueConnectionString")]string checkAppointmentStatusItem,
+            [Queue("sendappointmentreminderqueue", Connection = "SendAppointmentReminderQueueConnectionString")]CloudQueue sendAppointmentReminderQueue,
             ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function CheckAppointmentStatus processed: \n{myQueueItem}");
+            log.LogInformation($"C# Queue trigger function CheckAppointmentStatus processed: \n{checkAppointmentStatusItem}");
 
-            var data = JsonConvert.DeserializeObject<AppointmentReminderMessage>(myQueueItem);
-            
-            var url = GetEnvironmentVariable("recruiterUrl") + "api/InterviewAppointment/" + data.InterviewAppointmentId;
-            log.LogInformation($"\n3:------------------url:{url}------------------");
-            bool appointmentExist = false;
-            data.JobPositionName += " ST:" + DateTime.UtcNow.ToString();
+            var notificationData = JsonConvert.DeserializeObject<AppointmentReminderMessage>(checkAppointmentStatusItem);
+            var interviewAppointmentId = notificationData.InterviewAppointmentId;
+            var url = GetEnvironmentVariable("recruiterUrl") + "api/InterviewAppointment/CheckAppointmentStatus/";
+
+            bool appointmentCheck = false;
             try
             {
-                WebRequest request = WebRequest.Create(url);
+                WebRequest request = WebRequest.Create(url + interviewAppointmentId);
                 request.Credentials = CredentialCache.DefaultCredentials;
                 WebResponse response = request.GetResponse();
 
-                var statusCode = ((HttpWebResponse)response).StatusDescription;
-                log.LogInformation($"Status: {statusCode}");
-
-                Stream dataStream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(dataStream);
-                string responseFromServer = reader.ReadToEnd();
-                log.LogInformation($"responseFromServer: {responseFromServer}");
-                appointmentExist = bool.Parse(responseFromServer);
-
-                reader.Close();
+                appointmentCheck = ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK ? true : false;
                 response.Close();
             }
             catch (WebException ex)
@@ -50,28 +40,13 @@ namespace CheckAppointmentStatusFunc
                 log.LogError($"Message:{ex.Message}" +
                                 $"\nURL:{url}");
             }
-            data.JobPositionName += " ET:" + DateTime.UtcNow.ToString();
 
-            var message = JsonConvert.SerializeObject(data);
-            log.LogInformation($"\nMessage: \n{message}");
-            if (appointmentExist)
+            if (appointmentCheck)
             {
-                log.LogWarning($"\n3:------------------1-output-send------------------");
-                var queueMessage = new CloudQueueMessage(message);
-                await outputQueue.CreateIfNotExistsAsync();
-                await outputQueue.AddMessageAsync(
-                    queueMessage,
-                    timeToLive: null,
-                    initialVisibilityDelay: null,
-                    options: null,
-                    operationContext: null);
+                var queueMessage = new CloudQueueMessage(checkAppointmentStatusItem);
+                await sendAppointmentReminderQueue.CreateIfNotExistsAsync();
+                await sendAppointmentReminderQueue.AddMessageAsync(queueMessage);
             }
-            else
-            {
-                log.LogError(   $"\n3:------------------2-discard------------------" +
-                                $"\n3:------------------Appointment not found------------------");
-            }
-
         }
 
         public static string GetEnvironmentVariable(string name)
@@ -80,3 +55,21 @@ namespace CheckAppointmentStatusFunc
         }
     }
 }
+
+
+
+//WebRequest request = WebRequest.Create(url + interviewAppointmentId);
+//request.Credentials = CredentialCache.DefaultCredentials;
+//WebResponse response = request.GetResponse();
+
+//var statusCode = ((HttpWebResponse)response).StatusDescription;
+//log.LogInformation($"Status: {statusCode}");
+
+//Stream dataStream = response.GetResponseStream();
+//StreamReader reader = new StreamReader(dataStream);
+//string responseFromServer = reader.ReadToEnd();
+//log.LogInformation($"responseFromServer: {responseFromServer}");
+//appointmentExist = bool.Parse(responseFromServer);
+
+//reader.Close();
+//response.Close();
